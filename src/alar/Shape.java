9b9e2ea7afce5d428ec14676d4e212b29fe1922c
alaraasa <alar@alaraasa.ee>
2017-04-03 13:22:43 +0300

    Homework

---
+++ src/alar/Shape.java
@@ -0,0 +1,10 @@
+package alar;
+
+/**
+ * Created by AASA on 03.04.2017.
+ */
+public interface Shape {
+    public double getPerimeter();
+    public double getSurfaceArea();
+    public boolean getPossibility();
+}
