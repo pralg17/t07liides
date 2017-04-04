7608202f79de1fead5a02bb5af0b8e9a15f6383e
brigid-ly <brigidly.palkman@gmail.com>
2017-04-04 21:07:24 +0300

    Create Auto.java

---
+++ Auto.java
@@ -0,0 +1,11 @@
+public class Auto implements Liikur {
+
+   public void liigu() {
+      System.out.println("Auto liigub");
+   }
+
+   public void pargi() {
+      System.out.println("Auto pargib");
+   }
+
+}
