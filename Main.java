0d2492e01872f0c1c337e0a0fb616acf7da4c376
brigid-ly <brigidly.palkman@gmail.com>
2017-04-04 21:08:06 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+   public static void main(String args[]) {
+      Auto auto = new Auto();
+      auto.liigu();
+      auto.pargi();
+   }
+
+   /*
+
+    javac Auto.java Liikur.java Main.java
+    java Main
+
+    Auto liigub
+    Auto pargib
+
+   */
+
+}
