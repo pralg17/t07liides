e5948eca92ffa350ebeb18c656e1919f241d00c7
edgaseli <edgaseli@greeny.cs.tlu.ee>
2017-04-05 21:08:14 +0300

    homework

---
+++ Kasutus.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Kasutus{
+	public static void main(String[] arg){
+		Liides KeraRuumalaArvutaja = new KeraFunktsioon();
+		System.out.println(KeraRuumalaArvutaja.f(3));
+	}
+}
+
+/* [
+113.09733552923254
+*/
