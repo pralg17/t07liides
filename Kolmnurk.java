ef38b0ceac8601c996c879aa17184aa4f53c0904
MK <mrxkollo@users.noreply.github.com>
2017-04-09 15:20:12 +0300

    Kolmnurgale lisasin konstruktori

--- Kolmnurk.java
+++ Kolmnurk.java
@@ -1,11 +1,25 @@
 
 class Kolmnurk implements GeoLiides {
 
+
+    double alus;
+    double korgus;
+    double kaatet1;
+    double kaatet2;
+
+    public Kolmnurk(double alus, double korgus, double kaatet1, double kaatet2) {
+        this.alus = alus;
+        this.korgus = korgus;
+        this.kaatet1 = kaatet1;
+        this.kaatet2 = kaatet2;
+    }
+
+
     public double umbermoot(double... arv){
-        return arv[0] + arv[2] + arv[3];
+        return  alus + kaatet1 + kaatet2;
     }
 
     public double pindala(double... arv){
-        return (arv[0]*arv[1])/2;
+        return (alus * korgus)/2;
     }
 }
