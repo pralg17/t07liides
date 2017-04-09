b5a349e4d32377016ee7032f35f22db47bd39c4e
MK <mrxkollo@users.noreply.github.com>
2017-04-09 15:22:46 +0300

    Ruudu lisamine

---
+++ Ruut.java
@@ -0,0 +1,13 @@
+
+class Ruut implements GeoLiides {
+
+    public double pindala(double... arv) {
+        return arv[0] * arv[0];
+    }
+
+
+    public double umbermoot(double... arv) {
+        return 4.0 * arv[0];
+    }
+
+}
