a416b830287294c097ea4ff41c9e46cb2a11e659
Marko Kollo <markkoll@greeny.cs.tlu.ee>
2017-04-09 15:13:38 +0300

    Interface + Kolmnurk

---
+++ Kolmnurk.java
@@ -0,0 +1,11 @@
+
+class Kolmnurk implements GeoLiides {
+
+    public double umbermoot(double... arv){
+        return arv[0] + arv[2] + arv[3];
+    }
+
+    public double pindala(double... arv){
+        return (arv[0]*arv[1])/2;
+    }
+}
