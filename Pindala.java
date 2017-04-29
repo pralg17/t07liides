ccd0df094abd008f982c911f40cbbdeb7cc37e5e
XeniaBelor <https://XeniaBelor@github.com/XeniaBelor>
2017-04-29 17:34:49 +0300

    Kodunetoo

---
+++ Pindala.java
@@ -0,0 +1,5 @@
+public class Pindala implements Liides{
+	public double arvutamine(double a, double b){
+		return a*b;
+	}
+}
---
+++ Ymbermqqt.java
@@ -0,0 +1,5 @@
+public class Ymbermqqt implements Liides{
+	public double arvutamine(double a, double b){
+		return (2*a) + (2*b);
+	}
+}
