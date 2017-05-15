c878c45358b49c0590fc9e767564ecbb4eeb7c2a
alarvere <alarvere@greeny.cs.tlu.ee>
2017-05-15 10:59:45 +0300

    homework interface

---
+++ src/main/java/com/example/Rakendus.java
@@ -0,0 +1,36 @@
+import java.util.*;
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.beans.factory.annotation.Autowired;
+
+public class Rakendus {
+	public static int arvuta(Funktsioon fn, int arv){
+		return fn.f(arv);
+	}
+
+	public static void main(String[] arg){
+
+		Funktsioon yArvutaja=new Vorrand(5);
+		Funktsioon yxaArvutaja=new Vorrand2(5, 6);
+		System.out.println(arvuta(yArvutaja, 3));
+		System.out.println(arvuta(yxaArvutaja, 3));
+
+
+		//System.getProperties().put("server.port", 5555);
+		//SpringApplication.run(Rakendus.class, args);
+	}
+}
+
+//scl enable rh-maven33 bash
+//mvn package
+//java -jar target/boot1-1.0-SNAPSHOT.jar
+
+//Running RakendusTest
+//Tests run: 1, Failures: 0, Errors: 0, Skipped: 0, Time elapsed: 0.002 sec - in RakendusTest
+
+//Results :
+
+//Tests run: 1, Failures: 0, Errors: 0, Skipped: 0
+
