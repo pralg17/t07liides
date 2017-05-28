7e4de582a60141bcca323487a83fb018d4b76629
atsklem <atsklem@greeny.cs.tlu.ee>
2017-05-28 17:14:06 +0300

    Adding hw

---
+++ Interface.java
@@ -0,0 +1,3 @@
+public interface Interface{
+    public double f(double x);
+}
---
+++ Kuupjuur.java
@@ -0,0 +1,5 @@
+public class Kuupjuur implements Interface{
+    public double f(double x){
+        return Math.cbrt(x);
+    }
+}
